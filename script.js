010c448b0929a4748b39ee01443c1bd2e009b149
ParsuramBaral <parsurambaral42@gmail.com>
2024-08-18 19:33:27 +0545

    Updated Script Functionality
    Updated the Game vs Mode(computer)
    Added Feature of draw and optimized the logic of some functions.

--- script.js
+++ script.js
@@ -16,54 +16,81 @@ var win_condition = [
 ]
 
 
+const computerturn =() =>{
+   var remainingboxes = Array.from(box).filter(boxes => boxes.textContent === "");
+    if (remainingboxes.length >0){
+        index = Math.floor(Math.random()*remainingboxes.length);
+        randombox = remainingboxes[index];
+        randombox.textContent = "X";
+        click_count +=1;
+        winner_decider();
+        if (click_count === 9 && winner.innerText === "") {
+            draw.innerText = `Oops! The match was drawn`;
+            disablebox();
+        }
+    };
+};
 function clicker(){
     box.forEach((boxes) =>{
-    boxes.addEventListener("click", () =>{
+        boxes.addEventListener("click", () =>{
             if(playerO){
                 boxes.innerText = "O";
                 playerO= false;
-            } else{
-                boxes.innerText = "X";
-                playerO= true;
+                click_count +=1;
+                winner_decider();
+                if (click_count === 9 && winner.innerText === "") {
+                    draw.innerText = `Oops! The match was drawn`;
+                    disablebox();
+                }
+
+                if(winner.innerText === ""){
+                    setTimeout(() => {
+                        computerturn();
+                        playerO = true;
+                    }, 400);
+                }
             }
             boxes.disabled = true;  
-            click_count++;
-            winner_decider();
         });
     });
-};
+} 
 
 reset_game.addEventListener("click", ()=> {
     box.forEach((boxes) =>{
         boxes.innerText = "";
         boxes.disabled = false;
-    })
+    });
     winner.innerText = "";
     draw.innerText = "";
     playerO = true;
     click_count = 0;
-})
+});
 
 const disablebox = () => {
     box.forEach((boxes) =>{
         boxes.disabled = true;
-    })
+    });
 }
 
 clicker();
+
 const winner_decider = () => {
     win_condition.forEach((conditions) => {
-        boxvalue1= box[conditions[0]].innerText;
-        boxvalue2= box[conditions[1]].innerText;
-        boxvalue3= box[conditions[2]].innerText;
+        let boxvalue1= box[conditions[0]].innerText;
+        let boxvalue2= box[conditions[1]].innerText;
+        let boxvalue3= box[conditions[2]].innerText;
         if (boxvalue1 !=="" && boxvalue2 !=="" && boxvalue3 !=="") {
             if (boxvalue1 === boxvalue2 && boxvalue2 === boxvalue3) {
-                winner.innerText = `Congratulations ${boxvalue1} You have Won`;
-                disablebox();
-                } else if (click_count === 9){
-                    draw.innerText = `OPPS! The match was drawn`;
+                if(boxvalue1 === "O") { 
+                    winner.innerText = "Congratulations Player has Won";
                     disablebox();
+                    return; } 
+                else{
+                    winner.innerText = "OPPS! Computer has Won";
+                    disablebox();
+                    return;
                 }
+            }
         }
-    })
+    });    
 };
